b155a254a3c7fff62b4612089ba04e75692033e3
Ebrahim Mangera <ebrahimmangera9@gmail.com>
2023-10-23 01:02:32 +0000

    Task 0X04 Web_flask

--- models/state.py
+++ models/state.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-""" STATE MODULE """
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.base_model import Base, BaseModel
-from models.city import City
+"""This is the state class"""
+import os
 import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ CLASS STATE """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade='all, delete')
+    """This is the class for State
+    Attributes:
+        name: input name
+        cities = relationship between state and city tables.
+    """
 
-    @property
-    def cities(self):
-        """
-        LIST OF CITY
-        """
+    __tablename__ = 'states'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            'City', back_populates='state',
+            cascade='all, delete, delete-orphan')
 
-        records = models.storage.all()
-        res = []
-        for city in records.values():
-            if self.id == city.state_id:
-                res.append(city)
-        return res
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """returns list of Cities and some relationships"""
+            cities_instances = []
+            cities_dict = models.storage.all(models.City)
+            for key, value in cities_dict.items():
+                if self.id == value.state_id:
+                    cities_instances.append(value)
+            return cities_instances
